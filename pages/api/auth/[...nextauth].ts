eef6d3efe2f5a5abc072fe505c460b45ac4c779b
Shubham Saxena <shubh.lpu16@gmail.com>
2024-06-23 17:44:07 +0530

    add: next auth secret

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,32 @@
+import NextAuth from 'next-auth'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { PrismaClient } from '@prisma/client'
+import GoogleProvider from 'next-auth/providers/google'
+import GithubProvider from 'next-auth/providers/github'
+
+const prisma = new PrismaClient()
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma) as any,
+
+  providers: [
+    GoogleProvider({
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET as string,
+      httpOptions: {
+        timeout: 40000
+      }
+    }),
+    GithubProvider({
+      clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET as string
+    })
+  ],
+  session: {
+    strategy: 'jwt'
+  },
+  secret: process.env.NEXTAUTH_SECRET
+}
+
+// @ts-ignore
+export default NextAuth(authOptions)
